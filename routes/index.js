693e8025949af8fdef5bbf7937ba4321324fda19
anushkar <anushka.ranasinghe@grabone.co.nz>
2023-08-09 08:35:14 +1200

    Updated with Account and Blanace Route

--- routes/index.js
+++ routes/index.js
@@ -5,42 +5,64 @@ var cors = require("cors");
 let { Web3 } = require("web3");
 require("dotenv").config();
 
+let provider = new Web3.providers.HttpProvider(
+  `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+);
+const web3 = new Web3(provider);
+const address = "0xC8CD2BE653759aed7B0996315821AAe71e1FEAdF";
 /*
 "address": "0x9c59D33594bC1f6046d704246A6CF9B9D3EF24F5"
 */
+//Get Balance by address
+router.get("/balance/:address", async (req, res) => {
+  const address = req.params.address;
+  try {
+    let balance = await web3.eth.getBalance(address);
+    balance = web3.utils.fromWei(balance, "ether");
 
-router.get("/", function (req, res, next) {
-  let provider = new Web3.providers.HttpProvider(
-    `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-  );
-  const web3 = new Web3(provider);
+    res.json({ address, balance });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching balance" });
+  }
+});
+
+//Create account
+router.get("/create", async (req, res) => {
+  try {
+    const account = await web3.eth.accounts.create();
+    res.json({
+      address: account.address,
+      privateKey: account.privateKey,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error create account" });
+  }
+});
+
+router.get("/", (req, res) => {
   async function main() {
     try {
-      // Get the current block number from the network
-      const currentBlockNumber = await web3.eth.getBlockNumber();
-      console.log("Current block number:", currentBlockNumber);
+      //Get the current block number from the network
+      const balance = await web3.eth.getBalance(address);
 
-      // Get the list of accounts in the connected node (e.g., Ganache)
-      const accounts = await web3.eth.getAccounts();
+      // Get transaction count (nonce)
+      const nonce = await web3.eth.getTransactionCount(address);
 
-      // Send a transaction to the network and wait for the transaction to be mined.
-      // Note that sending a transaction with Ganache will cause it, in its default configuration, to min a new block.
-      const transactionReceipt = await web3.eth.sendTransaction({
-        from: accounts[0],
-        to: accounts[1],
-        value: web3.utils.toWei("0.001", "ether"),
+      // Get code (smart contract) at the address
+      const code = await web3.eth.getCode(address);
+
+      res.json({
+        address,
+        balance: web3.utils.fromWei(balance, "ether"),
+        //nonce,
+        code,
       });
-      console.log("Transaction Receipt:", transactionReceipt);
-
-      // Get the updated block number
-      const updatedBlockNumber = await web3.eth.getBlockNumber();
-      console.log("Updated block number:", updatedBlockNumber);
-      console.log(accounts);
     } catch (error) {
-      console.error("An error occurred:", error);
+      console.error(error);
     }
   }
   main();
 });
-
 module.exports = router;
